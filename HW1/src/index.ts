f865c25c221cf58b197af1b291c81e819da1f56c
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-12 15:09:19 +0100

    Update index.ts

--- HW1/src/index.ts
+++ HW1/src/index.ts
@@ -2,3 +2,28 @@
 function greetUser(name: string): void {
     console.log(`Привет, ${name}!`);
 }
+// Интерфейс Person
+interface Person {
+    name: string;
+    age: number;
+    city: string;
+}
+// Функция отображения информации о человеке
+function printPersonInfo(person: Person): void {
+    console.log(`Имя: ${person.name}, Возраст: ${person.age}, Город: ${person.city}`);
+}
+
+// Функция, возвращающая квадрат числа
+function squareNumber(num: number): number {
+    return num * num;
+}
+// Функция проверки чётности
+function isEven(num: number): boolean {
+    return num % 2 === 0;
+}
+
+// Интерфейс Student
+interface Student {
+    name: string;
+    grade: number;
+}
