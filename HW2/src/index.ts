0c200ff57f4dad73b9d69214ebe48502300c6a99
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-13 19:18:23 +0100

    Update index.ts

--- HW2/src/index.ts
+++ HW2/src/index.ts
@@ -20,3 +20,52 @@ function displayId(id: string | number): void {
 }
 id = "abc123";
 displayId(id); // ID: ABC123
+
+//Массив объектов и фильтрация
+type OrderStatus = "pending" | "shipped" | "delivered";
+
+interface Order {
+    orderId: string;
+    amount: number;
+    status: OrderStatus;
+}
+
+const orders: Order[] = [
+    { orderId: "A1", amount: 100, status: "pending" },
+    { orderId: "B2", amount: 250, status: "shipped" },
+    { orderId: "C3", amount: 300, status: "delivered" },
+    { orderId: "D4", amount: 120, status: "pending" }
+];
+
+function filterOrdersByStatus(orders: Order[], status: OrderStatus): Order[] {
+    return orders.filter(order => order.status === status);
+}
+
+console.log(filterOrdersByStatus(orders, "pending"));
+
+//Кортеж и работа с объектами
+type ProductInfo = [string, number, number]; 
+// [name, price, changeInStock]
+
+function updateStock(
+    inventory: Record<string, number>,
+    productInfo: ProductInfo
+): Record<string, number> {
+    const [name, , change] = productInfo;
+
+    if (inventory[name] !== undefined) {
+        inventory[name] += change;
+    } else {
+        inventory[name] = change;
+    }
+
+    return inventory;
+}
+let inventory = { 
+    "Laptop": 10, 
+    "Phone": 25 
+};
+const productInfo: ProductInfo = ["Laptop", 999.99, -2];
+
+console.log(updateStock(inventory, productInfo));
+//  { Laptop: 8, Phone: 25 }
