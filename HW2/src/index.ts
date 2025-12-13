6d734848dc1111ca784cfbab5196e6c8da21111d
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-13 19:08:54 +0100

    Update index.ts

--- HW2/src/index.ts
+++ HW2/src/index.ts
@@ -9,3 +9,14 @@ console.log(calculateTotal(100, 2, 0.1)); // 180
 
 
 //Union типы
+let id: string | number;
+
+function displayId(id: string | number): void {
+    if (typeof id === "string") {
+        console.log("ID:", id.toUpperCase());
+    } else {
+        console.log("ID:", id * 10);
+    }
+}
+id = "abc123";
+displayId(id); // ID: ABC123
