c6791c9ed03d06393a13a08ca50de7ef1f204577
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-15 18:15:04 +0100

    Update index.ts

--- HW4/src/index.ts
+++ HW4/src/index.ts
@@ -6,3 +6,15 @@ const sumEvenNumbers = (numbers: number[]): number => {
 // Пример использования
 const nums = [1, 2, 3, 4, 5, 6];
 console.log(sumEvenNumbers(nums)); // 12 (2 + 4 + 6)
+
+//интерфейс для функции, которая принимает строку и возвращает boolean
+interface StringToBooleanFunction {
+    (input: string): boolean;
+}
+
+// Пример функции: проверка на пустую строку
+const isEmpty: StringToBooleanFunction = (str) => str.trim().length === 0;
+
+// Использование
+console.log(isEmpty("")); // true
+console.log(isEmpty("Hello")); // false
