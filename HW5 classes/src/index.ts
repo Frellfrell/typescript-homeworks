dbd38b385248f85c3efcedd4a0e8fc7c567a8a3a
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-19 18:37:47 +0100

    Update index.ts

--- HW5 classes/src/index.ts
+++ HW5 classes/src/index.ts
@@ -11,3 +11,22 @@ class Animal {
     console.log("The animal makes a sound");
   }
 }
+class Dog extends Animal {
+  breed: string;
+
+  constructor(name: string, breed: string) {
+    super(name, "Dog");
+    this.breed = breed;
+  }
+
+  sound(): void {
+    console.log("The dog barks");
+  }
+}
+
+// Проверка
+const animal = new Animal("Generic", "Unknown");
+animal.sound();
+
+const dog = new Dog("Rex", "Labrador");
+dog.sound();
