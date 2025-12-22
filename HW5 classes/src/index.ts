96acfc7bfaf23322fa31a72650c5ea8f21c57b1b
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-22 20:33:55 +0100

    Update index.ts

--- HW5 classes/src/index.ts
+++ HW5 classes/src/index.ts
@@ -1,3 +1,4 @@
+//Класс Animal и наследник Dog
 class Animal {
   name: string;
   species: string;
@@ -30,3 +31,51 @@ animal.sound();
 
 const dog = new Dog("Rex", "Labrador");
 dog.sound();
+
+//Статическое свойство totalBooks
+class Book {
+  static totalBooks: number = 0;
+
+  addLibraryBook(): void {
+    Book.totalBooks += 1;
+  }
+  }
+
+// Проверка
+const book1 = new Book();
+
+const book2 = new Book();
+
+book1.addLibraryBook();
+book1.addLibraryBook();
+book2.addLibraryBook();
+
+console.log("Total books:", Book.totalBooks); // Вывод: Total books: 3
+
+//Переопределение конструктора (Vehicle → Motorcycle)
+class Vehicle {
+  make: string;
+  model: string;
+
+  constructor(make: string, model: string) {
+    this.make = make;
+    this.model = model;
+  }
+}
+
+class Motorcycle extends Vehicle {
+  type: string;
+
+  constructor(make: string, model: string, type: string) {
+    super(make, model);
+    this.type = type;
+  }
+}
+
+// Проверка
+const bike = new Motorcycle("Yamaha", "R1", "Sport");
+
+console.log(bike.make);   // Yamaha
+console.log(bike.model);  // R1
+console.log(bike.type);   // Sport
+
