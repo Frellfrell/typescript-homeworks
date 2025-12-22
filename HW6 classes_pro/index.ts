f040dfc13d93c3ab28687499a3246849515043e2
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-22 20:58:57 +0100

    Update index.ts

--- HW6 classes_pro/index.ts
+++ HW6 classes_pro/index.ts
@@ -18,3 +18,50 @@ const animals: Animal[] = [new Dog(), new Cat()];
 animals.forEach(animal => {
     console.log(animal.makeSound());
 });
+//Shape + Color Example
+abstract class Shape {
+    abstract calculateArea(): number;
+}
+
+abstract class ColoredShape extends Shape {
+    abstract color: string;
+}
+
+class ColoredCircle extends ColoredShape {
+    color = "Red";
+    radius: number;
+
+    constructor(radius: number) {
+        super();
+        this.radius = radius;
+    }
+
+    calculateArea(): number {
+        return Math.PI * this.radius ** 2;
+    }
+}
+
+class ColoredRectangle extends ColoredShape {
+    color = "Blue";
+    width: number;
+    height: number;
+
+    constructor(width: number, height: number) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+
+    calculateArea(): number {
+        return this.width * this.height;
+    }
+}
+
+const shapes: ColoredShape[] = [
+    new ColoredCircle(5),
+    new ColoredRectangle(4, 6)
+];
+
+shapes.forEach(shape => {
+    console.log(`Color: ${shape.color}, Area: ${shape.calculateArea()}`);
+});
