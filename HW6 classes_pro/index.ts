92a778406969e031f7572250efce4022f0f695c5
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-22 21:06:37 +0100

    Update index.ts

--- HW6 classes_pro/index.ts
+++ HW6 classes_pro/index.ts
@@ -65,3 +65,38 @@ const shapes: ColoredShape[] = [
 shapes.forEach(shape => {
     console.log(`Color: ${shape.color}, Area: ${shape.calculateArea()}`);
 });
+
+//Абстрактный класс Appliance
+abstract class Appliance {
+    abstract turnOn(): void;
+    abstract turnOff(): void;
+}
+class WashingMachine extends Appliance {
+    turnOn(): void {
+        console.log("Washing machine is ON");
+    }
+
+    turnOff(): void {
+        console.log("Washing machine is OFF");
+    }
+}
+
+class Refrigerator extends Appliance {
+    turnOn(): void {
+        console.log("Refrigerator is ON");
+    }
+
+    turnOff(): void {
+        console.log("Refrigerator is OFF");
+    }
+}
+
+const appliances: Appliance[] = [
+    new WashingMachine(),
+    new Refrigerator()
+];
+
+appliances.forEach(appliance => {
+    appliance.turnOn();
+    appliance.turnOff();
+});
