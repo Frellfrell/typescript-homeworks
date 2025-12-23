6a78e52ed01e68b77629e82f008d705b9e55ad8c
Frellfrell <ella.gubareva.24@gmail.com>
2025-12-23 21:59:14 +0100

    Update stringUtils.ts

--- HW7/src/stringUtils.ts
+++ HW7/src/stringUtils.ts
@@ -1,6 +1,6 @@
 export function capitalize(str: string): string {
   if (!str) return str;
-  return str[0].toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 export function reverseString(str: string): string {
