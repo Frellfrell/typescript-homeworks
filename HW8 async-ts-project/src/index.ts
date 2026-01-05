c3e0862542070117c271e3227c8b7905cd1fb2ae
Frellfrell <ella.gubareva.24@gmail.com>
2026-01-05 20:35:29 +0100

    Update index.ts

--- HW8 async-ts-project/src/index.ts
+++ HW8 async-ts-project/src/index.ts
@@ -1,3 +1,16 @@
 const delay = (ms: number): Promise<void> => {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
+
+const processSequentially = async () => {
+  console.log('Start');
+  await delay(1000);
+  console.log('First operation completed');
+  await delay(2000);
+  console.log('Second operation completed');
+  await delay(1500);
+  console.log('Third operation completed');
+  console.log('All operations completed');
+};
+
+processSequentially();
